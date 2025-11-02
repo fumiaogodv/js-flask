ea1dfc18cf6ea302880a235bedbbad9c1c8efebb
fumiao <we1213869@qq.com>
2025-11-02 18:14:43 +0800

    three

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
 from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 
+# 获取当前 app.py 文件所在的绝对路径
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///event.db"
+# 使用绝对路径拼接数据库文件地址
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "event.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+# ##################################################################
+#  关键修改部分：添加一个中间件来处理 URL 前缀
+# ##################################################################
+class PrefixMiddleware(object):
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        # 如果请求路径以指定的前缀开头，则移除前缀
+        # 例如，将 /hello/wuziqi 修改为 /wuziqi
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            # 如果不匹配前缀，直接返回 404
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This URL does not belong to the app.".encode()]
+
+# 将你的 Flask 应用包装在这个中间件中
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/hello')
+# ##################################################################
+
 
 # ------------------- 数据模型 -------------------
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20), nullable=False)#不为空
+    title = db.Column(db.String(20), unique=True,nullable=False)#不为空
     description = db.Column(db.String(120))
     date = db.Column(db.String(10))
 
@@ -64,7 +93,7 @@ def find_event(event_id):
         "date": event.date
     })
 
-@app.route("/api/events/change",methods=["POSt"])
+@app.route("/api/events/change",methods=["POST"])
 def change_event():
     id_info=request.get_json()
     event_id=id_info.get('id')
